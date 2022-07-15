a1a954154aef88ec640567d44109294f30dab69f
KeltonMad <keltonmadden@gmail.com>
2022-07-15 12:22:15 -0600

    interface

--- contracts/interfaces/ICollectionCoreV1.sol
+++ contracts/interfaces/ICollectionCoreV1.sol
@@ -9,10 +9,10 @@ pragma solidity ^0.8.0;
  */
 
 interface ICollectionCore {
-    function initialize(string calldata name, string calldata symbol, string calldata contractURI, string calldata tokenURI, uint supply, uint price, 
+    function initialize(string calldata name, string calldata symbol, string calldata contractURI, string calldata tokenURI, uint supply, uint price, uint8 primaryRoyaltyPercentage,
         uint8 maxPurchaseNumber, uint reserveNumber) external;
     function publisherWithdraw() external;
-    function adminWithdraw() external;
+    function adminWithdraw() external;                                                                       
     function mint(uint8 number, address recipient) external payable;
     function totalSupply() external returns (uint8);
     function pauseMint(bool) external;
