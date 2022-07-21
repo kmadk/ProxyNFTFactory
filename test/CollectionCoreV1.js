8744050506210c4dc05a6cdd7859d68e67e7f70b
KeltonMad <keltonmadden@gmail.com>
2022-07-21 09:01:44 -0600

    collectionCore sanity tests

--- test/CollectionCoreV1.js
+++ test/CollectionCoreV1.js
@@ -5,7 +5,7 @@ const { ethers } = require('hardhat')
 
 describe("CollectionCoreV1", () => {
 
-	let alice, bob, charlie
+	let alice, bob, charlie, alexandriaAdmin
 	let collectionCore
   const pow18 = BigNumber.from('1000000000000000000')
 
@@ -14,17 +14,23 @@ describe("CollectionCoreV1", () => {
     alice = accounts[0];
     bob = accounts[1];
     charlie = accounts[2];
+    alexandriaAdmin = accounts[3];
     const CollectionCore = await ethers.getContractFactory('CollectionCoreV1')
     collectionCore = await CollectionCore.deploy()
 	})
 
 	it("[sanity]: should initialize", async () => {
-    await collectionCore.initialize("Alexandria", "ALEX", {maxSupply: BigNumber.from('1000'), pow18, BigNumber.from("150"), maxPurchaseNumber: BigNumber.from('5'), })
+    await collectionCore.initialize("Alexandria", "ALEX", {maxSupply: BigNumber.from('1000'), price: pow18, primaryRoyaltyPercentage: BigNumber.from("150"), 
+      maxPurchaseNumber: BigNumber.from('5'), reserveNumber: BigNumber.from('0'), contractURI: "", baseURI: "", payoutAddress: alice.address, alexandriaAddress: alexandriaAdmin.address})
 	})
 
-  
+   
 	it("[sanity]: cannot initialize twice", async () => {
-    await 
+    await collectionCore.initialize("Alexandria", "ALEX", {maxSupply: BigNumber.from('1000'), price: pow18, primaryRoyaltyPercentage: BigNumber.from("150"), 
+      maxPurchaseNumber: BigNumber.from('5'), reserveNumber: BigNumber.from('0'), contractURI: "", baseURI: "", payoutAddress: alice.address, alexandriaAddress: alice.address})
+    await expectRevert(collectionCore.initialize("Alexandria", "ALEX", {maxSupply: BigNumber.from('1000'), price: pow18, primaryRoyaltyPercentage: BigNumber.from("150"),
+      maxPurchaseNumber: BigNumber.from('5'), reserveNumber: BigNumber.from('0'), contractURI: "", baseURI: "", payoutAddress: alice.address, alexandriaAddress: alice.address}),
+      "Initializable: contract is already initialized")
 	})
 
 
